53cc602037ffbd52fc458084b76b318a6757cf69
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-25 21:18:37 +0530

    Add project files.

---
+++ Method Overloding Sequence of Parameter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloding_Sequence_of_Parameter
+{
+
+    class name
+    {
+        public void show(int x,string y)
+        {
+            Console.WriteLine("hello");
+        }
+        public void show(string x,int y)
+        {
+            Console.WriteLine("hiii Akshay saini");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            name obj = new name();
+            obj.show("hi",10);
+            Console.ReadLine();
+        }
+    }
+}
